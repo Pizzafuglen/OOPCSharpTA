6511bf89c1be4578b05581e39494889badce8f18
Pizzafuglen <Crose19@student.sdu.dk>
2022-11-14 00:49:08 +0100

    Completed yet another task

---
+++ Temperature/CelciusFahrenheit.cs
@@ -0,0 +1,14 @@
+﻿namespace Temperature;
+
+public class CelciusFahrenheit
+{
+    public static double ConvertCelcius(double temp)
+    {
+        return 32 + temp * 9 / 5;
+    }
+
+    public static double ConvertFahrenheit(double temp)
+    {
+        return (temp - 32) * 5 / 9;
+    }
+}
---
+++ Temperature/PlayingField.cs
@@ -0,0 +1,13 @@
+﻿namespace Temperature;
+
+public class PlayingField
+{
+    public static void Main(string[] args)
+    {
+        var celciusToFarh = CelciusFahrenheit.ConvertCelcius(20);
+        
+        Console.WriteLine(celciusToFarh);
+        
+        Console.WriteLine(CelciusFahrenheit.ConvertFahrenheit(68));
+    }
+}
